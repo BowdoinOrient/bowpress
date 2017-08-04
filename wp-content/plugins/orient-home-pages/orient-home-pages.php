d55a801d661b078497a4dd0fe6f642555470e280
James Little <littleguy23@gmail.com>
2017-08-04 04:20:58 +0000

    Advertising

--- wp-content/plugins/orient-home-pages/orient-home-pages.php
+++ wp-content/plugins/orient-home-pages/orient-home-pages.php
@@ -147,3 +147,13 @@ function home_render($module_name, $article_index = NULL) {
 		return $output;
 	}
 }
+
+add_filter( 'the_posts', function( $posts, \WP_Query $query )
+{
+    if( $pick = $query->get( '_shuffle_and_pick' ) )
+    {
+        shuffle( $posts );
+        $posts = array_slice( $posts, 0, (int) $pick );
+    }
+    return $posts;
+}, 10, 2 );
