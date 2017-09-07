72b9e55bece681f7e4aec0a73fe539c467ff6599
James Little <littleguy23@gmail.com>
2017-09-07 02:02:11 +0000

    Overhaul page styles and add sidebar

--- wp-content/themes/orient-theme/page.php
+++ wp-content/themes/orient-theme/page.php
@@ -6,21 +6,30 @@ if (have_posts()) :
 		the_post();
 	?>
 
-	<?php if(!is_in_front_page_tree()): ?>
+	<?php
+		// If this isn't the home page, do things a little differently
+		if(!is_in_front_page_tree()):
+	?>
 
-		<header class="archive-header series-archive-header">
-			<h1 class="series-archive-header-title"><?php the_title() ?></h1>
+		<header class="page-header">
+			<h1><?php the_title() ?></h1>
 		</header>
 
 	<?php endif; ?>
 
-	<article>
-		<div class="content">
+	<div class="content">
+		<aside>
+			<?php if(get_field("sidebar")) {
+				the_field("sidebar");
+			} ?>
+		</aside>
+
+		<article>
 			<?php the_content(); ?>
-		</div>
-	</article>
+		</article>
+	</div>
 
 <?php
 	endwhile;
 endif;
-get_footer(); 
+get_footer();
