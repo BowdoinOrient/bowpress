6531547e1e5d64b853ec96272e94363247f03bef
James Little <littleguy23@gmail.com>
2017-10-04 02:23:42 +0000

    Home pages hotfix

--- wp-content/plugins/orient-home-pages/home-page-script.js
+++ wp-content/plugins/orient-home-pages/home-page-script.js
@@ -10,24 +10,28 @@ jQuery(document).ready(function() {
 
 function generateSelects() {
 	var inputTemplate = jQuery("#input-template");
-	var count = jQuery('input[name=file]:checked').attr('data-count');
-	jQuery("#article-selects").html("");
-	for (var i=0; i<count; i++) {
-		var selected = articleIds[i];
+	if(inputTemplate) {
+		var count = jQuery('input[name=file]:checked').attr('data-count');
+		jQuery("#article-selects").html("");
+		for (var i=0; i<count; i++) {
+			var selected = articleIds[i];
 
-		inputTemplate.find("option").removeAttr("selected");
+			inputTemplate.find("option").removeAttr("selected");
 
-		if(selected !== "") {
-			inputTemplate.find("[value=" + selected  + "]").attr("selected", "selected");
+			if(selected !== "") {
+				inputTemplate.find("[value=" + selected  + "]").attr("selected", "selected");
+			}
+
+			jQuery("#article-selects").append(i+1 + ": " + inputTemplate.html() + "<br>");
 		}
-
-		jQuery("#article-selects").append(i+1 + ": " + inputTemplate.html() + "<br>");
 	}
 }
 
 function updateImage() {
 	var img = jQuery("#orient_homepage_image")
 	var templateName = jQuery("input[name=file]:checked").attr('value');
-	templateName = templateName.substring(0, templateName.length - 4);
-	img.attr('src', '/wp-content/themes/orient-theme/homepages/' + templateName + '.png');
+	if(templateName) {
+		templateName2 = templateName.substring(0, templateName.length - 4);
+		img.attr('src', '/wp-content/themes/orient-theme/homepages/' + templateName2 + '.png');
+	}
 }
