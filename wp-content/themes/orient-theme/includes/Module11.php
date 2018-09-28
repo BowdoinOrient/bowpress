e8a1f0b695dd23f66b3d382d82c6b370b9431d11
James Little <littleguy23@gmail.com>
2018-09-27 21:05:46 -0400

    Delete unused files

--- wp-content/themes/orient-theme/includes/Module11.php
+++
@@ -1,5 +0,0 @@
-<article class="module module-11">
-	<h2 class="kicker"><?php the_kicker(); ?></h2>
-
-	<h1 class="article-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
-</article>
