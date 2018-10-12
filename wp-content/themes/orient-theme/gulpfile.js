e0d388607cd22cf8ce506fdbceed05b377b3344e
James Little <3058200+jameslittle230@users.noreply.github.com>
2018-10-11 21:45:55 -0400

    Update Gulp to 4.0.0 (#18)
    Gives us security fixes

--- wp-content/themes/orient-theme/gulpfile.js
+++ wp-content/themes/orient-theme/gulpfile.js
@@ -1,28 +1,39 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-clean-css');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var minifyCss = require("gulp-clean-css");
+var autoprefixer = require("gulp-autoprefixer");
 
-gulp.task('default', function() {
-
-
-});
-
-gulp.task('sass', function () {
+gulp.task("sass", function(done) {
   setTimeout(function() {
-  return gulp.src('./sass/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['> 2%'],
-      cascade: false
-    }))
-    .pipe(minifyCss({
-        keepSpecialComments: 1
-    }))
-    .pipe(gulp.dest('./'));
+    return gulp
+      .src("./sass/style.scss")
+      .pipe(sass().on("error", sass.logError))
+      .pipe(
+        autoprefixer({
+          browsers: ["> 2%"],
+          cascade: false
+        })
+      )
+      .pipe(
+        minifyCss({
+          keepSpecialComments: 1
+        })
+      )
+      .pipe(gulp.dest("./"));
   }, 100);
+  done();
 });
 
-gulp.task('watch', ['sass'], function () {
-    gulp.watch('./sass/**/*.scss', ['sass'] );
-});
+gulp.task(
+  "default",
+  gulp.series("sass", function(done) {
+    done();
+  })
+);
+
+gulp.task(
+  "watch",
+  gulp.series("sass", function() {
+    gulp.watch("./sass/**/*.scss", ["sass"]);
+  })
+);
