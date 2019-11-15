d96acb65889593a344ec5c2a93c8ad8b95d55187
Steven Xu <30362574+stevestar888@users.noreply.github.com>
2019-11-15 00:41:04 -0500

    Add files via upload

---
+++ static/149-fall-sports/149-fall-sports/golf-combined/app.js
@@ -0,0 +1,34 @@
+var ctx = document.getElementById("golf").getContext('2d');
+
+var original = Chart.defaults.global.legend.onClick;
+Chart.defaults.global.legend.onClick = function(e, legendItem) {
+  update_caption(legendItem);
+  original.call(this, e, legendItem);
+};
+
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["Bowdoin Inv.", "Polar Bear Shootout",	"Bobcat Inv.", "Mt. Holyoke Inv.", "Williams Inv.", "George Phinney Classic",	"NESCAC Fall Qualifier", "CBB Champs."],
+    datasets: [{
+      label: 'Women',
+      backgroundColor: "#CAB1D1",
+      data: [44, 57, 93, 102,	0, 213, 190, 0],
+    }, {
+      label: 'Men',
+      backgroundColor: "#8974A5",
+      data: [103,	0, 64, 0, 78, 0, 69, 39],
+    }]
+  },
+
+  options: {
+    scales: {
+        yAxes: [{
+        ticks: {
+               min: 0,
+               max: 150,
+            },  
+        }]
+    }
+}
+});
