55752082fe61d99dd461bf3052967eca840bf8b6
Steven Xu <weareallhalfminotaur@gmail.com>
2020-02-28 00:33:06 -0500

    initial charts for 2020-spring-sports

---
+++ static/2020-spring-sports/league-position/app.js
@@ -0,0 +1,83 @@
+let LIGHT_GREEN ="#A5CFAB" 
+let DARK_GREEN = "#4A6E62"
+let YELLOW = "#f1c40f"
+let DARK_RED = "#692031" 
+let DARK_PURPLE = "#8974A5"
+let LIGHT_BLUE = "#48639C"
+let DARK_BLUE = "#1D3461"
+
+W_BBALL = [null, null, null, null, null, 1, 1, 1, 2, 2, 2, 2]
+M_BBALL = [null, null, null, null, null, 4, 9, 9, 10, 8, 9, 9]
+W_HOCKEY = [1, 3, 3, 3, 3, 5, 6, 7, 7, 7, 7, 7]
+M_HOCKEY = [4, 5, 5, 5, 5, 4, 5, 4, 5, 8, 8, 5]
+
+let WEEKS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+
+var config = {
+  type: 'line',
+  data: {
+    labels: WEEKS,
+    datasets: [{
+      label: "W Basketball",
+      backgroundColor: YELLOW,
+      borderColor: YELLOW,
+      data: W_BBALL,
+      fill: false,
+      lineTension: 0
+    }, {
+      label: "M Basketball",
+      fill: false,
+      backgroundColor: LIGHT_GREEN,
+      borderColor: LIGHT_GREEN,
+      data: M_BBALL,
+      lineTension: 0
+    }, {
+      label: "W Hockey",
+      fill: false,
+      backgroundColor: LIGHT_BLUE,
+      borderColor: LIGHT_BLUE,
+      data: W_HOCKEY,
+      lineTension: 0
+    }, {
+      label: "M Hockey",
+      fill: false,
+      backgroundColor: DARK_RED,
+      borderColor: DARK_RED,
+      data: M_HOCKEY,
+      lineTension: 0
+    }]
+  },
+  options: {
+    responsive: true,
+    title:{
+      display:true,
+    },
+    tooltips: {
+      // mode: 'index', // toggle between showing all labels or individual
+      intersect: false,
+    },
+   hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Week'
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Ranking'
+        },
+      }]
+    }
+  }
+};
+
+var ctx = document.getElementById("league-position").getContext("2d");
+var myLine = new Chart(ctx, config);
---
+++ static/2020-spring-sports/record-overall/app.js
@@ -0,0 +1,54 @@
+let LIGHT_GREEN ="#A5CFAB" 
+let DARK_GREEN = "#4A6E62"
+let YELLOW = "#f1c40f"
+let DARK_RED = "#692031" 
+let DARK_PURPLE = "#8974A5"
+let LIGHT_BLUE = "#48639C"
+let DARK_BLUE = "#1D3461"
+
+let WIN = DARK_GREEN
+let LOSS = DARK_PURPLE
+let TIE = YELLOW
+
+let OVERALL = [70, 62, 4]
+let M_TEAMS = [30, 35, 1]
+let W_TEAMS = [40, 27, 3]
+
+
+var ctx = document.getElementById("record-overall").getContext('2d');
+var chartData = {
+  labels: [
+    "Win",
+    "Loss",
+    "Tie"
+  ],
+  datasets: [{
+    data: OVERALL,
+    backgroundColor: [
+      WIN,
+      LOSS,
+      TIE
+    ],
+  }, {
+    data: M_TEAMS,
+    backgroundColor: [
+      WIN,
+      LOSS,
+      TIE
+    ],
+  },{
+    data: W_TEAMS,
+    backgroundColor: [
+      WIN,
+      LOSS,
+      TIE
+    ],
+  },
+   {
+    data: [0]
+  }]
+};
+var pieChart = new Chart(ctx, {
+  type: 'pie',
+  data: chartData
+})
