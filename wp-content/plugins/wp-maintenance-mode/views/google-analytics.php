190d9f31428e8931b0f3f8291746be69826b0e42
James Little <littleguy23@gmail.com>
2023-11-28 19:09:50 -0500

    Remove wordpress core

--- wp-content/plugins/wp-maintenance-mode/views/google-analytics.php
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=<?php esc_attr_e($ga_code); ?>"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', '<?php esc_attr_e($ga_code); ?>', <?php echo wp_json_encode($ga_options); ?>);
-</script>
--- wp-content/plugins/wp-optimize/templates/info-message.php
+++
@@ -1,4 +0,0 @@
-<?php if (!defined('ABSPATH')) die('No direct access.'); ?>
-<div class="wpo_info">
-	<?php echo $message; ?>
-</div>
