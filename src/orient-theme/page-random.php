09514890b23fdac083c1a064cfc64fb98107d98a
James Little <littleguy23@gmail.com>
2023-12-11 21:22:07 -0500

    Fix random again

--- src/orient-theme/page-random.php
+++ src/orient-theme/page-random.php
@@ -1,15 +1,22 @@
 <?php
 
-$args = array(
+$query = new WP_Query(array(
     'orderby' => 'rand',
     'numberposts' => 1,
     'post_type' => array(
         'post'
     ),
-);
-query_posts($args);
-foreach ($posts as $post) {
-    $link = get_permalink($post);
+));
+
+$link = "/";
+
+if ($query->have_posts()) {
+    while ($query->have_posts()) {
+        $query->the_post();
+        $link = get_permalink();
+
+    }
 }
+
 wp_redirect($link . '?random=true', 307);
 exit;
